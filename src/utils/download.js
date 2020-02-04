df279e912a78aa464dea5eac92d74f10101518f4
Noah Jadallah <noah.jadallah@gmx.net>
2020-02-04 21:46:33 +0100

    download auth

--- src/utils/download.js
+++ src/utils/download.js
@@ -1,39 +1,47 @@
-import FileSaver from 'file-saver';
+import FileSaver from 'file-saver'
+import fetch from 'node-fetch'
 
 // a case when we need to silently download a file using Javascript, and prompt to save it afterwards
-function promptToSaveBlob({ content, name, downloadUrl }) {
+function promptToSaveBlob(_ref) {
+  var content = _ref.content,
+    name = _ref.name,
+    downloadUrl = _ref.downloadUrl
+
   if (downloadUrl) {
-    const iframeId = 'oc-fm--filemanager-download-iframe';
-    let iframeDOMNode = document.getElementById(iframeId);
-
-    if (!iframeDOMNode) {
-      iframeDOMNode = document.createElement('iframe');
-      iframeDOMNode.style.display = 'none';
-      iframeDOMNode.id = iframeId;
-      document.body.appendChild(iframeDOMNode);
-    }
-
-    iframeDOMNode.src = downloadUrl;
+    fetch(downloadUrl, {
+      method: 'GET',
+      headers: {
+        Authorization: localStorage.getItem('token')
+      }
+    }).then(response => {
+      response.blob().then(blob => {
+        const url = window.URL.createObjectURL(blob)
+        const a = document.createElement('a')
+        a.style.display = 'none'
+        a.href = url
+        a.download = name
+        document.body.appendChild(a)
+        a.click()
+        window.URL.revokeObjectURL(url)
+      })
+    })
   } else {
-    const blob = new Blob([content], { type: 'octet/stream' });
-    FileSaver.saveAs(blob, name);
+    var blob = new Blob([content], { type: 'octet/stream' })
+    _fileSaver2.default.saveAs(blob, name)
   }
 }
 
 // a case when we trigger a direct download in browser
 // used in google drive' connector
 function triggerHiddenForm({ downloadUrl, target = '_self' }) {
-  const form = document.createElement("form");
-  form.action = downloadUrl;
-  form.target = target;
-  form.method = 'GET';
+  const form = document.createElement('form')
+  form.action = downloadUrl
+  form.target = target
+  form.method = 'GET'
 
-  document.body.appendChild(form);
-  form.submit();
-  document.body.removeChild(form);
+  document.body.appendChild(form)
+  form.submit()
+  document.body.removeChild(form)
 }
 
-export {
-  promptToSaveBlob,
-  triggerHiddenForm
-};
+export { promptToSaveBlob, triggerHiddenForm }
